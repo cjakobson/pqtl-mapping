01bfbff2b8ff3e055c1ee9b2f2f14829314a47d9
cjakobson <christopher.jakobson@gmail.com>
2024-05-29 14:12:20 -0700

    Update plot_transgression.m

--- plotting/plot/plot_transgression.m
+++ plotting/plot/plot_transgression.m
@@ -11,7 +11,7 @@ function []=plot_transgression(dependency_directory,output_directory)
     
     [input_mat,strain_names,ydj_names,strain_merge_idx,rm_idx,yjm_idx,f6_idx,orf_names,strain_index]=...
         parse_raw_abundance(dependency_directory,output_directory);
-        
+    
     
     [transgressive_mat]=calculate_transgression(dependency_directory,output_directory);
 
