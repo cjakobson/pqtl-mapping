a77a7a7600313042a11c086644a6b5357d8be8f2
cjakobson <christopher.jakobson@gmail.com>
2024-06-26 09:39:31 -0700

    Update plot_suppressor_sim.m

--- plotting/plot/plot_suppressor_sim.m
+++ plotting/plot/plot_suppressor_sim.m
@@ -13,75 +13,72 @@ function []=plot_suppressor_sim(plot_offset,dependency_directory,output_director
     
 
     load([dependency_directory 'phasedGenotype.mat'])
-    hapGenotypes=phasedGenotype;
+    hap_genotypes=phasedGenotype;
     clear phasedGenotype
 
-    mkdir('genotypes')
 
-    [nHaps,nPoly]=size(hapGenotypes);
+    [~,n_poly]=size(hap_genotypes);
 
     rng(0)
 
 
     %make an example trait with N loci
     %do range of suppressors
-    nLoci=50;
-    nSuppressors=3;
+    n_loci=50;
+    n_suppressors=3;
 
 
-    betaMean=0.5;   %mean and SD for underlying fitness effects
-    betaSD=0.1;
+    beta_mean=0.5;   %mean and SD for underlying fitness effects
+    beta_sd=0.1;
 
 
-    hSquared=0.8;
+    h_squared=0.8;
 
 
-    causalIdx=randperm(nPoly,nLoci);
+    causal_idx=randperm(n_poly,n_loci);
 
-    vBeta=zeros(nPoly,1);
+    v_beta=zeros(n_poly,1);
 
-    for m=1:length(causalIdx)
+    for m=1:length(causal_idx)
 
-        tempLocus=causalIdx(m);
+        temp_locus=causal_idx(m);
 
-        tempBeta=betaMean+randn*betaSD*(-1)^ceil(rand*2);     %randomly assign magnitude (uniform 0 to 0.2)
-        tempBeta=tempBeta*(-1)^ceil(rand*2);    %randomly assign +/-
+        temp_beta=beta_mean+randn*beta_sd*(-1)^ceil(rand*2);     %randomly assign magnitude (uniform 0 to 0.2)
+        temp_beta=temp_beta*(-1)^ceil(rand*2);    %randomly assign +/-
 
-        vBeta(tempLocus)=tempBeta;          %for RM genotypes
+        v_beta(temp_locus)=temp_beta;          %for RM genotypes
 
     end
 
     %hard code the modified effect
-    pivotLocus=10191;
-    vBeta(pivotLocus)=2*betaMean;
+    pivot_locus=10191;
+    v_beta(pivot_locus)=2*beta_mean;
 
 
-    suppIdx=randperm(nPoly,nSuppressors);
+    supp_idx=randperm(n_poly,n_suppressors);
 
-    for i=0:nSuppressors
+    for i=0:n_suppressors
 
-        tempGenotypes=hapGenotypes;
+        temp_genotypes=hap_genotypes;
 
         if i>0
 
-            toZero=suppIdx(1:i);
+            to_zero=supp_idx(1:i);
 
-            suppHaps=logical(sum(hapGenotypes(:,toZero)==1,2));
+            supp_haps=logical(sum(hap_genotypes(:,to_zero)==1,2));
 
-            sum(suppHaps)
-
-            tempGenotypes(suppHaps,pivotLocus)=0;
+            temp_genotypes(supp_haps,pivot_locus)=0;
 
         end
 
 
-        phenotypes{i+1}=tempGenotypes*vBeta;
+        phenotypes{i+1}=temp_genotypes*v_beta;
 
         phenotypes{i+1}=(phenotypes{i+1}-mean(phenotypes{i+1},'omitnan'))./...
             std(phenotypes{i+1},[],'omitnan');
 
-        noiseMag=1-hSquared;
-        noise=noiseMag*(randn(length(phenotypes{i+1}),1)).*...
+        noise_mag=1-h_squared;
+        noise=noise_mag*(randn(length(phenotypes{i+1}),1)).*...
             (-1).^ceil(rand(length(phenotypes{i+1}),1)*2);
 
         phenotypes{i+1}=phenotypes{i+1}+noise;
@@ -94,11 +91,11 @@ function []=plot_suppressor_sim(plot_offset,dependency_directory,output_director
 
         subplot(2,8,plot_offset+m)
         hold on
-        toPlot{1}=phenotypes{i}(hapGenotypes(:,pivotLocus)==1);
-        toPlot{2}=phenotypes{i}(hapGenotypes(:,pivotLocus)==-1);
-        easyBox(toPlot)
+        to_plot{1}=phenotypes{i}(hap_genotypes(:,pivot_locus)==1);
+        to_plot{2}=phenotypes{i}(hap_genotypes(:,pivot_locus)==-1);
+        easy_box(to_plot)
         ylim([-2 2])
-        [h p]=ttest2(toPlot{1},toPlot{2});
+        [h p]=ttest2(to_plot{1},to_plot{2});
         text(1.5,1.5,num2str(p))
         title(['nSuppressors = ' num2str(i-1)])
         xticks(1:2)
